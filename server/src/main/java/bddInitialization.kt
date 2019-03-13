b711680289c3511a222204e0b48a4c82b22bacbc
Unknown <acheng@intechinfo.fr>
2019-03-13 16:09:26 +0100

    Revert "Revert "Merge remote-tracking branch 'origin/master' into develop""
    This reverts commit 06133b70c744439d1e9118dd7c7d5278e0978efe.

---
+++ server/src/main/java/bddInitialization.kt
@@ -0,0 +1,35 @@
+package utils
+
+import java.sql.Connection
+import java.sql.DriverManager
+
+fun main(list : List<String>) {
+    initBdd();
+}
+
+fun initBdd() {
+
+    val url = utils.BDD_URL;
+
+    try {
+        var conn: Connection? = DriverManager.getConnection(url)
+
+        if (conn != null) {
+
+            var createTableReq = "CREATE IF NOT EXISTS users(\n" +
+                    "id integer PRIMARY KEY AUTOINCREMENT, \n" +
+                    "pseudo varchar(35) UNIQUE, \n" +
+                    "level integer DEFAULT 0, \n" +
+                    "points integer DEFAULT 0 \n" +
+                    ");"
+
+            var createTable = conn.prepareStatement(createTableReq)
+
+            createTable.executeUpdate()
+
+            conn.close()
+        }
+    } catch(e : Exception) {
+        e.printStackTrace()
+    }
+}
